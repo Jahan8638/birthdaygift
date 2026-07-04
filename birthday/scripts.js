3c23944645a89a98afdd040bbf3cc2febfa2dc7c
Jahan8638 <jahanuddinstudent@gmail.com>
2026-07-04 22:39:17 +0530

    Delete birthday directory

--- birthday/scripts.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("surpriseBtn").addEventListener("click", function () {
-    alert("🎂 Jahan is wishing you a day filled with happiness and unforgettable memories!. May your days and future years be filled with happines 🎉");
-});
