11f9a193c2915685c64ead181cbe01c39d9cd878
Jahan8638 <jahanuddinstudent@gmail.com>
2026-07-04 22:41:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("surpriseBtn").addEventListener("click", function () {
+    alert("🎂 Jahan is wishing you a day filled with happiness and unforgettable memories!. May your days and future years be filled with happines 🎉");
+});
